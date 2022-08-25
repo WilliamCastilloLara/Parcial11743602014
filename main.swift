6edacbd8158c087937f4be20751b4e469e38123c
William Castillo <99561702+WilliamCastilloLara@users.noreply.github.com>
2022-08-25 13:13:50 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,14 @@
+//William Eduardo Castillo Lara
+//carne 17-43-60-2014
+var num1: Float = 15.03
+var num2: Float = 95.19
+
+var suma = num1 + num2
+print("La suma es: \(suma)")
+
+
+var num3: Double = 3.0006
+var num4: Double = 5.0003
+
+var multiplicacion = num3 * num4
+print("la multiplicacion es: \(multiplicacion)")
